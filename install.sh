fd4280c891bbb8c48243a7e26754b57e9148ff3b
ad1tya2 <74638765+ad1tya2@users.noreply.github.com>
2020-12-12 20:22:40 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt install python3 -y
+sudo apt install zip -y
+pip install clint
+pip install requests
+git clone https://github.com/ad1tya2/mcserver-installer-and-optimizer.git
+mv ./mcserver-installer-and-optimizer.git/* ./
+rm -r mcserver-installer-and-optimizer.git
+chmod +x mcinstaller.sh
+echo -e "Should the minecraft server downloader-optimizer be started Now? (y/n)\n"
+read opt
+if [ "$opt" == "y" ] || [ "$opt" == "Y" ]
+then
+	echo Starting!
+	./mcinstaller.sh
+fi
