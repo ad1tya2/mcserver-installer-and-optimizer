a2575766c5c721c933ee2862523c95bd16ebb21e
ad1tya2 <74638765+ad1tya2@users.noreply.github.com>
2020-12-12 20:47:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo apt install python3 -y
+echo Installing requirements!
+{sudo apt install python3 -y
 sudo apt install zip -y
 pip install clint
 pip install requests
+} &> /dev/null
 git clone https://github.com/ad1tya2/mcserver-installer-and-optimizer.git
 mv ./mcserver-installer-and-optimizer/* ./
 rm -r mcserver-installer-and-optimizer
-sed -i -e 's/\r$//' mcinstaller.sh
 chmod +x mcinstaller.sh
 echo -e "Should the minecraft server downloader-optimizer be started Now? (y/n)\n"
 read opt
