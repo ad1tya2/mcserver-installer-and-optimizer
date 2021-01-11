a5452d9e2e2bf02440531784dc4b722f8d3157d7
ad1tya2 <74638765+ad1tya2@users.noreply.github.com>
2021-01-11 20:07:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo Installing requirements!
-{sudo apt install python3 -y
-sudo apt install zip -y
-pip install clint
-pip install requests
+{sudo apt install python3 zip -y
+pip install clint requests
 } &> /dev/null
 git clone https://github.com/ad1tya2/mcserver-installer-and-optimizer.git
 mv ./mcserver-installer-and-optimizer/* ./
