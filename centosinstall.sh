7bf94bec9147dc87db092cf7f9b2e3b2ab31a5de
ad1tya2 <74638765+ad1tya2@users.noreply.github.com>
2021-01-11 18:52:42 +0530

    Create centosinstall.sh

---
+++ centosinstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo Installing requirements!
+{sudo yum install python3 -y
+sudo yum install zip -y
+pip install clint
+pip install requests
+} &> /dev/null
+git clone https://github.com/ad1tya2/mcserver-installer-and-optimizer.git
+mv ./mcserver-installer-and-optimizer/* ./
+rm -r mcserver-installer-and-optimizer
+chmod +x mcinstaller.sh
+echo -e "Should the minecraft server downloader-optimizer be started Now? (y/n)\n"
+read opt
+if [ "$opt" == "y" ] || [ "$opt" == "Y" ]
+then
+	echo Starting!
+	./mcinstaller.sh
+fi
