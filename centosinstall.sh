2bef3d0df999f3e43fa4ee95db63c318397dcea1
ad1tya2 <74638765+ad1tya2@users.noreply.github.com>
2021-01-11 20:17:55 +0530

    Update centosinstall.sh

--- centosinstall.sh
+++ centosinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo Installing requirements!
 yum install epel-release zip python3 python-pip git java-1.8.0-openjdk -y
-pip install clint requests
+pip3 install clint requests
 git clone https://github.com/ad1tya2/mcserver-installer-and-optimizer.git
 mv ./mcserver-installer-and-optimizer/* ./
 rm -r mcserver-installer-and-optimizer
