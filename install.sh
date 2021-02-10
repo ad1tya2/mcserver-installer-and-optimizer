86c8aa5120ab7ff50aca0145b8b9e21c7eaf8117
ad1tya2 <74638765+ad1tya2@users.noreply.github.com>
2021-02-10 13:16:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo Installing requirements!
-{sudo apt install python3 zip openjdk-8-jre-headless -y
+{
+sudo apt install python3 zip openjdk-8-jre-headless -y
 pip3 install clint requests
 } &> /dev/null
 git clone https://github.com/ad1tya2/mcserver-installer-and-optimizer.git
